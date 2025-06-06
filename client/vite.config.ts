fea9beb12c897f2fec859172610ceb7242037b58
dape <dani.pejakovic@gmail.com>
2025-06-06 13:19:19 +0000

    adding icons to dapellm

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -52,8 +52,8 @@ export default defineConfig(({ command }) => ({
       },
       includeAssets: [],
       manifest: {
-        name: 'LibreChat',
-        short_name: 'LibreChat',
+        name: 'DaPeLLM',
+        short_name: 'DaPeLLM',
         start_url: '/',
         display: 'standalone',
         background_color: '#000000',
